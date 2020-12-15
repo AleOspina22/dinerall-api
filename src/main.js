02ff910985f03b93b7068f84fcb85b37f613d4ba
Dewith Miramón <dewithmiramon@outlook.com>
2020-12-15 09:11:51 -0500

    Creación de la app con vue

--- src/main.js
+++ src/main.js
@@ -2,25 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import vueRouter from 'vue-router'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '@/assets/css/main.css'
-
-Vue.use(vueRouter)
 
 Vue.config.productionTip = false
 
+/* eslint-disable no-new */
 new Vue({
-  router,
   el: '#app',
+  router,
   components: { App },
   template: '<App/>'
 })
-
-
-
-
-
-
-
