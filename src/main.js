66fc94d6951d68ebedff2c816b2bad2eb117cb42
Seigan94 <kzlopez94@gmail.com>
2020-12-15 10:04:52 -0500

    Main, router y server

--- src/main.js
+++ src/main.js
@@ -2,14 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import vueRouter from 'vue-router'
 import router from './router'
+Vue.use(vueRouter)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
+  el: '#app',
   components: { App },
   template: '<App/>'
 })
